d21e25da3b1776dc30fb0c470f0f6c659e1887c1
shell1010 <amin.dev03@gmail.com>
2025-05-12 21:35:46 +0100

    Fixed where sometimes player won't have to change

--- src/cogs/verification.py
+++ src/cogs/verification.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
-import discord
 
-from discord.ext import commands
-from discord import app_commands
-from typing import TYPE_CHECKING
+import asyncio
 import random
+from typing import TYPE_CHECKING
 
+import discord
+from discord import app_commands
+from discord.ext import commands
 from discord.types import user
 
 from ..classes import VerifyTicket
 
 if TYPE_CHECKING:
     from ..bot import AQWVerifier
+
 
 class Verification(commands.Cog):
     def __init__(self, bot: AQWVerifier) -> None:
@@ -21,16 +23,25 @@ class Verification(commands.Cog):
     @app_commands.describe(username="Your AQW username")
     async def verify(self, ctx: discord.Interaction, username: str):
         user_check = await self.bot.verify_username(username)
+        await ctx.response.defer(thinking=True)
         if user_check:
             ccid = await self.bot.get_ccid(username)
             inventory = await self.bot.get_inventory(ccid)
-            
+
             while True:
                 specific_item = random.choice(inventory)
-                if not self.bot.verify_item(username, specific_item['strName']):
+                if not self.bot.verify_item(username, specific_item["strName"]):
                     break
-            await ctx.response.send_message(view=VerifyTicket(self.bot, ccid, username, specific_item),embed=discord.Embed(title="Verification 50% complete", description=f"Now that we've verified your username. We will now verify that this account is yours. We ask you to equip this specific item\n\n`{specific_item['strName']}`\n\nPlease equip this item in-game then wait 30s-1m before clicking verify again."), ephemeral=True)
-            
+                await asyncio.sleep(5)
+            await ctx.followup.send(
+                view=VerifyTicket(self.bot, ccid, username, specific_item),
+                embed=discord.Embed(
+                    title="Verification 50% complete",
+                    description=f"Now that we've verified your username. We will now verify that this account is yours. We ask you to equip this specific item\n\n`{specific_item['strName']}`\n\nPlease equip this item in-game then wait 30s-1m before clicking verify again.",
+                ),
+                ephemeral=True,
+            )
+
     @app_commands.command(name="sync", description="Syncs commands to the server.")
     async def sync(self, ctx: discord.Interaction):
         await self.bot.tree.sync()
